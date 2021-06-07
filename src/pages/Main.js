916ea7c1b7dc2bcbfaeedbef08cde7bddd3ba7be
Serhii Voloshyn <garant-t@ukr.net>
2021-06-07 10:19:32 +0300

    created react project

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './main.scss';
 
-function App() {
+function Main() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          my App
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Main;
