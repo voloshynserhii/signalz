e0242c7b435b28f668d068d3d92f55f58c595a57
Serhii Voloshyn <garant-t@ukr.net>
2021-06-09 14:16:01 +0300

    some fixes in reviews section

--- src/components/ReviewsSection/components/ReviewSlide.js
+++ src/components/ReviewsSection/components/ReviewSlide.js
@@ -1,24 +1,26 @@
-import '../reviews-section.scss';
+import "../reviews-section.scss";
 
 const ReviewSlide = (props) => {
-    return (
-        <div className="review">
-            <h5>{props.title}</h5>
-            <p>{props.text}</p>
-            <div className="flex-between-center review-bottom">
-                <div className="flex-between-center">
-                    <img src={props.image} alt={props.title}/>
-                    <div className="review-name">
-                        <div className="bold-small">{props.name}</div>
-                        <div className="bold-gray">{props.position}</div>
-                    </div>
-                </div>
-                <div>
-                    <img src={props.starImage} alt={props.title}/>
-                </div>
-            </div>
+  return (
+    <div className="review">
+      <div className="review-content">
+        <h5>{props.title}</h5>
+        <p>{props.text}</p>
+      </div>
+      <div className="flex-between-center review-bottom">
+        <div className="flex-between-center">
+          <img src={props.image} alt={props.title} />
+          <div className="review-name">
+            <div className="bold-small">{props.name}</div>
+            <div className="bold-gray">{props.position}</div>
+          </div>
         </div>
-    )
-}
+        <div>
+          <img src={props.starImage} alt={props.title} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ReviewSlide;
