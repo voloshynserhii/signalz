d680117bc0c23ac251de19e8b8326ee187023045
Max <max@maxs-air.home>
2021-06-12 15:39:03 +0100

    section title update

--- src/components/SliderSection/SliderSection.js
+++ src/components/SliderSection/SliderSection.js
@@ -4,7 +4,9 @@ import "./slider-section.scss";
 const SliderSection = () => {
   return (
     <div className="slider" id="features">
+      <div className="title-section">
       <h2>Your followers can copy your portfolio in 3 steps</h2>
+      </div>
       <div className="slider-wrapper">
         <SliderBlock />
       </div>
