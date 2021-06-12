7140fdf0765ec11d904911185195746cf580a3a8
Max <max@maxs-air.home>
2021-06-12 15:48:55 +0100

    faqs done

--- src/components/QuestionsSection/data.js
+++ src/components/QuestionsSection/data.js
@@ -1,38 +1,38 @@
 const DROPDOWN_DATA = [
     {
         id: 'a01',
-        title: "Lorem ipsum dolor sit amet consctetur adipiscing",
-        hiddenText: "Lorem ipsum adipiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+        title: "How do I earn with Signalz?",
+        hiddenText: "With Signalz, you earn through performance fees on your trades and by issuing your own portfolio token.",
         active: true
     },
     {
         id: 'a02',
-        title: "Lorem ipsum dolor sit amet consctetur adipiscing",
-        hiddenText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim nostrud.",
+        title: "Can I create my own token?",
+        hiddenText: "Yes, you can create a token, which your followers will purchase as they buy into your portfolio of cryptocurrencies.",
         active: false
     },
     {
         id: 'a03',
-        title: "Lorem ipsum dolor sit amet consctetur adipiscing",
-        hiddenText: "Lorem ipsum dolor sit sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+        title: "How do you notify followers about trades?",
+        hiddenText: "Followers will be updated through in app messages or through telegram, whenever you wish to update them about your trades.",
         active: false
     },
     {
         id: 'a04',
-        title: "Lorem ipsum dolor sit amet consctetur adipiscing",
-        hiddenText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+        title: "Can I tap into Signals audience?",
+        hiddenText: "Yes, you can definitely access or members. Strong traders will be advertised at the top of our trading leaderboard.",
         active: false
     },
     {
         id: 'a05',
-        title: "Lorem ipsum dolor sit amet etur adipiscing",
-        hiddenText: "Lorem ipsum dolor sit amet, consectetur aiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+        title: "How do I share my portfolio?",
+        hiddenText: "Sharing is as easy as creating a web-link to your portfolio through the app, and sharing it wherever you prefer.",
         active: false
     },
     {
         id: 'a06',
-        title: "Lorem amet consctetur adipiscing",
-        hiddenText: "Lorem ipsum dor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+        title: "Are you planning a token sale?",
+        hiddenText: "Yes, Signalz will reward early users with our own utility token, which will earn the platform's performance fee.",
         active: false
     },
     // {
